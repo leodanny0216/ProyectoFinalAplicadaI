0e75001a7453d6ff32f502bb1c9f43017f6c0455
Leodanny Maria Mercedes <leodanny_maria@ucne.edu.do>
2025-04-05 03:20:07 -0400

    Cree mi Index.razo en la carpeta Insumopage  y le agregue sus componentes

--- ProyectoFinalAplicadaI/Data/ApplicationDbContext.cs
+++ ProyectoFinalAplicadaI/Data/ApplicationDbContext.cs
@@ -1,8 +1,14 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ProyectoFinalAplicadaI.Models;
 
 namespace ProyectoFinalAplicadaI.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
+    public DbSet<Clientes> Clientes { get; set; }
+    public DbSet<Compras> Compras { get; set; }
+    public DbSet<ComprasDetalle> ComprasDetalles { get; set; }
+    public DbSet<Insumos> Insumos { get; set; }
+    public DbSet<Reclamos> Reclamos { get; set; }
 }
