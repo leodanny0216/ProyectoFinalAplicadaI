9c2c1837435f7bff49c96c730d16f51916740a1b
Leodanny Maria Mercedes <leodanny_maria@ucne.edu.do>
2025-04-05 03:03:02 -0400

    Ise la clase Reclamos  y le agrege los  componentes

---
+++ ProyectoFinalAplicadaI/Models/Reclamos.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinalAplicadaI.Models;
+
+public class Reclamos
+{
+    [Key]
+    public int ReclamoId { get; set; }
+    public DateTime FechaReclamo { get; set; }
+    [ForeignKey("ClienteId")]
+    public int ClienteId { get; set; }
+    [Required(ErrorMessage = "Se requiere una breve descripción del Reclamo.")]
+    public string? Descripcion { get; set; }
+    [Required(ErrorMessage = "Se requiere ingresar la Acción tomada.")]
+    public string? AccionTomada { get; set; }
+}
