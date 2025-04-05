9d9c79394d37d57cccc7783c8d9d36a75fdc51f9
Leodanny Maria Mercedes <leodanny_maria@ucne.edu.do>
2025-04-05 03:02:34 -0400

    Ise la clase Insumos y le agrege los  componentes

---
+++ ProyectoFinalAplicadaI/Models/Insumos.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinalAplicadaI.Models;
+
+public class Insumos
+{
+    [Key]
+    public int InsumosId { get; set; }
+    [Required(ErrorMessage = "Este campo es obligaotio")]
+    [RegularExpression("^[a-zA-Z ]+$", ErrorMessage = "El Nombre debe contener solo letras.")]
+    public string? NombreInsumo { get; set; }
+
+    public int Cantidad { get; set; }
+    public DateTime fechaRegistro { get; set; }
+    [Required(ErrorMessage = "Este campo es obligaotio")]
+    public string? Descripcion { get; set; }
+
+    [Required(ErrorMessage = "Debe especificar el precio")]
+    [Range(1.00, float.MaxValue, ErrorMessage = "EL precio debe ser mayor que 1.00")]
+    public float Precio { get; set; }
+}
