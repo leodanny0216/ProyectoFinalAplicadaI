52f36b0cad352d947e7d2b6b914d2f070ed48c9c
Leodanny Maria Mercedes <leodanny_maria@ucne.edu.do>
2025-04-05 03:03:49 -0400

    Ise la clase Compras con Detalles y le agrege los  componentes

---
+++ ProyectoFinalAplicadaI/Models/ComprasDetalle.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinalAplicadaI.Models;
+
+public class ComprasDetalle
+{
+    [Key]
+    public int ComprasDetalleId { get; set; }
+    [ForeignKey("InsumoId")]
+    public int InsumoId { get; set; }
+    [Required(ErrorMessage = "Debe especificar la cantidad a comprar")]
+    [Range(5, int.MaxValue, ErrorMessage = "La cantidad minima de productos a comprar es 5")]
+    public int Cantidad { get; set; }
+    public string? ImagenURL { get; set; }
+}
