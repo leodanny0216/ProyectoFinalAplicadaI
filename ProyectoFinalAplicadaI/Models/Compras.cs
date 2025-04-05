5f8a0564b04726c2bbbb1a6be3f2beef5ee841ab
Leodanny Maria Mercedes <leodanny_maria@ucne.edu.do>
2025-04-05 03:01:50 -0400

    Ise la clase Compras y le agrege los  componentes

---
+++ ProyectoFinalAplicadaI/Models/Compras.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinalAplicadaI.Models;
+
+public class Compras
+{
+    [Key]
+    public int ComprasId { get; set; }
+    [Required(ErrorMessage = "Debe seleccionar un usario")]
+    public DateTime FechaCompra { get; set; }
+    [Required(ErrorMessage = "Agregue una descripcion a esta compra")]
+    public string? Descripcion { get; set; }
+    [ForeignKey("ComprasId")]
+    public ICollection<ComprasDetalle> ComprasDetalle { get; set; } = new List<ComprasDetalle>();
+
+}
